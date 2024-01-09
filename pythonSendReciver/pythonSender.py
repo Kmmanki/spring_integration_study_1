20aa5fde52ded72c992dcd92e1985b8dc29d86bd
Kim mangi <aksrl25@mgail.com>
2024-01-09 18:04:36 +0900

    sender 응답 추가

--- pythonSendReciver/pythonSender.py
+++ pythonSendReciver/pythonSender.py
@@ -1,4 +1,5 @@
 import socket, time
+from threading import Thread
 
 host="localhost"
 port=10001
@@ -6,7 +7,17 @@ port=10001
 #Scoket Open
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sock.connect((host, port))
-# sock.send()
+
+def recvFromServer():
+    global sock
+    while True:
+        data = sock.recv(100)
+        msg = data.decode() # 읽은 데이터 디코딩
+        print('recv msg:', msg)
+
+t1 = Thread(target=recvFromServer)
+t1.start()
+
 while True:
     msg = input("MSG: ")
     # print(msg + )
